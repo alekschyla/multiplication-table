1fb0fcd744d78562db736ceecd805e590f0bb708
Aleksandra Chyła <aleks.chyla@gmail.com>
2018-11-29 19:50:44 +0100

    Print multiplication table in console.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function printMultiplicationTable(size) {
     for (let i = 1; i <= size; i++) {
         var result = "";
         for (let j = 1; j <= size; j++) {
-            result += i * j + " ";
+            result += i + " x " + j + " = " + i * j + "   ";
         }
         console.log(result);
     }
