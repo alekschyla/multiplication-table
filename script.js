22a92af49f670942997a5a02ee29724076bb1c17
Aleksandra Chyła <aleks.chyla@gmail.com>
2018-12-05 19:19:20 +0100

    Code clean up.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-document.getElementById("multiplicationTableContainer").innerHTML = printMultiplicationTable(10);
+document.getElementById("multiplicationTable").innerHTML = createMultiplicationTableContent(10);
 
-function printMultiplicationTable(size) {
-    var line = "";
-    for (let i = 1; i <= size; i++) {
-        var result = "";
-        for (let j = 1; j <= size; j++) {
-            result += "<td>" + i + " x " + j + " = " + i * j + "</td>";
+function createMultiplicationTableContent(tableSize) {
+    let tableContent = "";
+    for (let i = 1; i <= tableSize; i++) {
+        let rowContent = "";
+        for (let j = 1; j <= tableSize; j++) {
+            let cellContent = i + " x " + j + " = " + i * j;
+            rowContent += createCell(cellContent);
         }
-        line += "<tr>" + result + "</tr>";
+        tableContent += createRow(rowContent);
     }
-    return line;
+    return tableContent;
+}
+
+function createCell(cellContent) {
+    return "<td>" + cellContent + "</td>";
+}
+
+function createRow(rowContent) {
+    return "<tr>" + rowContent + "</tr>";
 }
 
